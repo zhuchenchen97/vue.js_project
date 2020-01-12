442906fbffe09ecd570a9d968bd2150479aed842
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-11 17:24:51 -0800

    router

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view/> <!--   //显示的是当前路由地址所对应的内容 -->
   </div>
 </template>
 
@@ -12,12 +11,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home.vue'
+import List from '@/pages/list/List.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/', 
+      name: 'Home',
+      component: Home
+    },{
+    	path:'/list',
+    	name: 'List',
+    	component: List
     }
   ]
 })
