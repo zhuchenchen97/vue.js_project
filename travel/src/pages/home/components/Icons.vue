37f69f83c453711cfa95d8bab0702ff797cdfcbf
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-14 11:21:04 -0800

    recommend1

--- travel/src/pages/home/components/Icons.vue
+++ travel/src/pages/home/components/Icons.vue
@@ -79,34 +79,36 @@ export default {
 	.icons >>> .swiper-container
 		height: 0
 		padding-bottom: 50% // 宽高比为2
-	.icon
-		position: relative
-		overflow: hidden
-		float: left			
-		width: 25%
-		height: 0
-		padding-bottom: 25% // 宽度与高度一样 padding-bottom相对于父元素的width值
-		.icon-img
-			position: absolute
-			top: 0
-			left: 0
-			right: 0
-			bottom: .44rem
-			box-sizing: border-box
-			padding: .1rem
-			.icon-img-content
-				display: block
-				margin: 0 auto
-				height: 100%
-		.icon-desc
-			position: absolute
-			left: 0
-			right: 0
-			bottom: 0	
-			line-height: .44rem	
-			height: .44rem
-			text-align: center		
-			color: $darkTextColor
-			ellipsis()
+	.icons
+		margin-top: .1rem
+		.icon
+			position: relative
+			overflow: hidden
+			float: left			
+			width: 25%
+			height: 0
+			padding-bottom: 25% // 宽度与高度一样 padding-bottom相对于父元素的width值
+			.icon-img
+				position: absolute
+				top: 0
+				left: 0
+				right: 0
+				bottom: .44rem
+				box-sizing: border-box
+				padding: .1rem
+				.icon-img-content
+					display: block
+					margin: 0 auto
+					height: 100%
+			.icon-desc
+				position: absolute
+				left: 0
+				right: 0
+				bottom: 0	
+				line-height: .44rem	
+				height: .44rem
+				text-align: center		
+				color: $darkTextColor
+				ellipsis()
 				
 </style>
