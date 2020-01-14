4f7524eb97881aeaaea93e9bb073622f5500aa96
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-14 15:11:54 -0800

    axios1

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
 	name: 'Home',
 	components:{
@@ -22,6 +23,18 @@ export default {
 		HomeIcons : HomeIcons,
 		HomeRecommend : HomeRecommend,
 		HomeWeekend: HomeWeekend
+	},
+	methods: {
+        getHomeInfo() {
+        	axios.get('/static/mock/index.json') // 请求一个url
+        	    .then(this.getHomeInfoSuccess)
+        },
+        getHomeInfoSuccess(res) {
+        	console.log(res)
+        }
+	},
+	mounted () {
+		this.getHomeInfo()
 	}
 }
 </script>
