ba5678a7232c314f8423384fac7873e2c3a1b96f
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-15 10:38:36 -0800

    city-list

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search></city-search>        
+        <city-search></city-search>
+        <city-list></city-list>        
     </div>
 
 </template>
@@ -9,11 +10,13 @@
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
 export default {
 	name: 'City',
     components: {
         CityHeader : CityHeader,
-        CitySearch : CitySearch
+        CitySearch : CitySearch,
+        CityList : CityList
     }
 }	
 </script>
