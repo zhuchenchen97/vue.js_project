76aaa7a9cde75d8f6d207eadc925f9bd8b877594
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-20 14:05:08 -0800

    city-keepalive

--- travel/src/App.vue
+++ travel/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/> <!--   //显示的内容是当前路由地址所对应的内容 在index.js中配置-->
+    <keep-alive>  // 下一次进路由的时候不需要重新渲染组件
+        <router-view/> <!--   //显示的内容是当前路由地址所对应的内容 在index.js中配置-->
+    </keep-alive>
   </div>
 </template>
 
