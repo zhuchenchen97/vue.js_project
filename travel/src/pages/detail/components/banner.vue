a24e8c31b59533858ad710c743bdedd33e751b2f
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-22 15:33:59 -0800

    banner finish

--- travel/src/pages/detail/components/banner.vue
+++ travel/src/pages/detail/components/banner.vue
@@ -1,19 +1,43 @@
 <template>
-    <div class="banner">
-        <img class="banner-img" src="http://imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg"/>
-        <div class="banner-info">
-            <div class="banner-tittle">AAAAA景区</div>
-            <div class="banner-number">
-                <span class="iconfont banner-icon">&#xe647;</span>
-                39
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="http://imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg"/>
+            <div class="banner-info">
+                <div class="banner-tittle">AAAAA景区</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe647;</span>
+                    39
+                </div>
             </div>
         </div>
+        <common-gallery :imgs="imgs" 
+                        v-show="showGallery"
+                        @close="handleGalleryClose"
+        ></common-gallery>
     </div>
 </template>
 
 <script>
+import CommonGallery from 'common/gallery/Gallery'
 export default {
-    name: 'DetailBanner'
+    name: 'DetailBanner',
+    data () {
+        return {
+            showGallery: false,
+            imgs: ['http://imgs.qunarzz.com/p/tts2/1811/1f/ec1030a0e6845c02.jpg_180x120_75656c79.jpg','http://imgs.qunarzz.com/vs_ceph_vs_tts/45adc913-5fc7-4d83-9e49-0227c6902801.jpg_180x120_e23be2dc.jpg']
+        }
+    },
+    methods: {
+        handleBannerClick () {
+           this.showGallery = true;
+        },
+        handleGalleryClose () {
+            this.showGallery = false;
+        }
+    },
+    components: {
+        CommonGallery
+    }
 }
 </script>
 
